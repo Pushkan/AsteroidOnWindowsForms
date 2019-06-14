e8bad32c599a3ecadc65d08e6feaadb9f1836733
Pushkan <pushkan13@gmail.com>
2019-06-14 14:37:15 +0300

    -added class BaseObject -added class Star:BaseObject -added class Game

---
+++ HomeWork2-1_FromZheleznyak/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HomeWork2_1_FromZheleznyak
+{
+    /// <summary>
+    /// 1. Добавить свои объекты в иерархию объектов, чтобы получился красивый задний фон, похожий на полет в звездном пространстве.
+    /// 2. * Заменить кружочки картинками, используя метод DrawImage.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+            Game.Init(form);
+            form.Show();
+            Game.Draw();
+            Application.Run(form);
+
+        }
+    }
+}
---
+++ HomeWork2-1_FromZheleznyak/Star.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+namespace HomeWork2_1_FromZheleznyak
+{
+    class Star:BaseObject
+    {
+        public Star(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+        Image baseImage = Image.FromFile("octopus.png");
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(baseImage, Pos);
+        }
+
+    public override void Update()
+        {
+            Pos.X = Pos.X - Dir.X;
+            Pos.Y = Pos.Y - Dir.Y;
+            if (Pos.X < 0) Dir.X = -Dir.X;
+            if (Pos.X > Game.Width) Dir.X = -Dir.X;
+            if (Pos.Y < 0) Dir.Y = -Dir.Y;
+            if (Pos.Y > Game.Height) Dir.Y = -Dir.Y;
+        }
+
+    }
+}
